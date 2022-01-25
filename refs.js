8d60066a03b5d51678fbeebd56fc16a88dac5c9c
Samantha Sunduza <73644813+Samantha853@users.noreply.github.com>
2022-01-25 13:55:52 +0200

    Add files via upload

---
+++ refs.js
@@ -0,0 +1,6 @@
+/*** HTML and CSS ***/
+https://www.inyourdoghousegifts.com/pages/inventory-csv-file-format-explained
+https://www.w3schools.com/howto/howto_js_filter_elements.asp
+/*** --- JS (JavaScript) ***/
+https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_filter_elements
+https://www.w3schools.com/js/default.asp
